a4ac25449ca3973ef48e4a5d6b21bd290b6c1d18
そだ20 <jomin398@gmail.com>
2023-02-28 09:42:42 +0000

    fixed Error, when used it with MMDLoader.

--- src/ZipLoader.js
+++ src/ZipLoader.js
@@ -1,130 +1,87 @@
 /**
  * @author Takahiro / https://github.com/takahirox
  */
-
+import * as THREE from 'three';
 import JSZip from 'jszip';
 
-
-function ZipLoader( manager ) {
-
-	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
-
-}
-
-function checkJSZipAvailability( onError ) {
-
-	if ( typeof JSZip === 'undefined' ) {
-
-		var error = new Error( 'THREE.ZipLoader: Import JSZip https://stuk.github.io/jszip/' );
-
-		if ( onError !== undefined ) {
-
-			onError( error );
-			return false;
-
-		} else {
-
-			throw error;
-
-		}
-
+class ZipLoader {
+	constructor(manager) {
+		this.manager = (manager !== undefined) ? manager : THREE.DefaultLoadingManager;
 	}
-
-	return true;
-
-}
-
-Object.assign( ZipLoader.prototype, {
-
-	constructor: ZipLoader,
-
-	load: function ( url, onProgress, onError ) {
-
-		if ( ! checkJSZipAvailability( onError ) ) return;
-
-		var scope = this;
-
-		var promise = JSZip.external.Promise;
-
-		var baseUrl = 'blob:' + THREE.LoaderUtils.extractUrlBase( url );
-
-		return new promise( function ( resolve, reject ) {
-
-			var loader = new THREE.FileLoader( scope.manager );
-			loader.setResponseType( 'arraybuffer' );
-			loader.load( url, resolve, onProgress, reject );
-
-		} ).then( function ( buffer ) {
-
-			return JSZip.loadAsync( buffer );
-
-		} ).then( function ( zip ) {
-
-			var fileMap = {};
-
-			var pendings = [];
-
-			for ( var file in zip.files ) {
-
-				var entry = zip.file( file );
-
-				if ( entry === null ) continue;
-
-				pendings.push( entry.async( 'blob' ).then( function ( file, blob ) {
-
-					fileMap[ baseUrl + file ] = URL.createObjectURL( blob );
-
-				}.bind( this, file ) ) );
-
+	/** Errors that can be thrown */
+	#errors = {
+		JSZIPREFError: class extends ReferenceError {
+			constructor(message) {
+				super(message);
+				this.name = "ZipLoader.JSZIPREFError";
 			}
-
-			return promise.all( pendings ).then( function () {
-
-				return fileMap;
-
-			} );
-
-		} ).then( function ( fileMap ) {
-
-			return {
-
-				urlResolver: function ( url ) {
-
-					return fileMap[ url ] ? fileMap[ url ] : url;
-
-				},
-
-				find: function ( query ) {
-
-					if ( typeof query === 'string' ) {
-
-						query = new RegExp( query.replace( /\./g, '\\.' ) );
-
-					}
-
-					var files = [];
-
-					for ( var key in fileMap ) {
-
-						if ( key.match( query ) !== null ) {
-
-							files.push( key );
-
-						}
-
-					}
-
-					return files;
-
-				}
-
-			};
-
-		} ).catch( onError );
-
+		}
 	}
 
-} );
+	#checkJSZipAvailability(onError) {
+		if (typeof JSZip === 'undefined') {
+			var error = new this.#errors.JSZIPREFError('Import JSZip https://stuk.github.io/jszip/');
+			if (onError !== undefined) {
+				onError(error);
+				return false;
+			} else {
+				throw error;
+			}
+		}
+		return true;
+	}
+	/**
+	 * @param {Object} zip zip object from jszip unzip function.
+	 * @returns {Object} fileMap
+	 */
+	async #makeblobURLs(url, zip) {
+		var fileMap = {};
+		var pendings = [];
+		var baseUrl = 'blob:' + THREE.LoaderUtils.extractUrlBase(url);
+		for (var file in zip.files) {
+			var entry = zip.file(file);
+			if (entry === null) continue;
+			pendings.push(entry.async('blob').then(function (file, blob) {
+				fileMap[baseUrl + file] = URL.createObjectURL(blob);
+			}.bind(this, file)));
+		}
+		await Promise.all(pendings);
+		console.log(fileMap)
+		return fileMap;
+	}
+	load(url, onProgress, onError) {
+		if (!this.#checkJSZipAvailability(onError)) return;
+		var scope = this;
+		// var promise = JSZip.external.Promise;
 
-
+		return new Promise(function (resolve, reject) {
+			var loader = new THREE.FileLoader(scope.manager);
+			loader.setResponseType('arraybuffer');
+			loader.load(url, resolve, onProgress, reject);
+		})
+			.then((buffer) => JSZip.loadAsync(buffer))
+			.then(zip => this.#makeblobURLs(url, zip))
+			.then((fileMap) => ({
+				getFileList: fileMap,
+				urlResolver: function (url) {
+					url = url.replace('\\', '/');
+					console.log(
+						`THREE.ZipLoader.urlResolver: Resolving Url...\n${url}`);
+					return fileMap[url] ? fileMap[url] : url;
+				},
+				find: function (query) {
+					if (typeof query === 'string') {
+						query = new RegExp(query.replace(/\./g, '\/.'));
+					}
+					var files = [];
+					for (var key in fileMap) {
+						if (key.match(query) !== null) {
+							files.push(key);
+						}
+					}
+					return files;
+				}
+			})).catch(onError);
+	}
+}
 export { ZipLoader };
--- src/ZipLoadingManager.js
+++ src/ZipLoadingManager.js
@@ -1,61 +1,52 @@
+import * as THREE from 'three';
+import { ZipLoader } from './ZipLoader.js';
 /**
+ * zip file loader for threejs.
  * @author Takahiro / https://github.com/takahirox
+ * @extends THREE.LoadingManager
  */
-
-function ZipLoadingManager( onLoad, onProgress, onError ) {
-
-	THREE.LoadingManager.call( this, onLoad, onProgress, onError );
-
-}
-
-ZipLoadingManager.prototype = Object.assign( Object.create( THREE.LoadingManager.prototype ), {
-
-	constructor: ZipLoadingManager
-
-} );
-
-ZipLoadingManager.uncompress = function ( url, exts, onLoad, onProgress, onError ) {
-
-	var manager = new THREE.ZipLoadingManager( onLoad, onProgress, onError );
-
-	if ( url.match( /\.zip$/ ) === null ) return Promise.resolve( { urls: [ url ], manager: manager } );
-
-	if ( ! Array.isArray( exts ) ) exts = [ exts ];
-
-	var str = '(';
-
-	for ( var i = 0, il = exts.length; i < il; i ++ ) {
-
-		if ( i > 0 ) str += '|';
-
-		str += exts[ i ].replace( '.', '\\.' );
-
+class ZipLoadingManager extends THREE.LoadingManager {
+	constructor(onLoad, onProgress, onError) {
+		super(onLoad, onProgress, onError)
 	}
-
-	str += ')$';
-
-	return new THREE.ZipLoader( manager ).load( url ).then( function ( zip ) {
-
-		var files = zip.find( new RegExp( str, 'i' ) );
-
-		if ( files.length === 0 ) {
-
-			return Promise.reject( new Error( 'THREE.ZipLoadingManager: No ' + exts.join( ', ' ) + ' file in ' + url ) );
-
+	/** Errors that can be thrown */
+	#errors = {
+		ENOENT: class extends Error {
+			constructor(exts, url) {
+				super(`No ${exts.join(', ')} file in ${url}`);
+				this.name = "ZipLoadingManager.ENOENT";
+			}
 		}
-
-		manager.setURLModifier( zip.urlResolver );
-
-		return {
-
-			urls: files,
-			manager: manager
-
-		};
-
-	} ).catch( onError );
-
-};
-
-
+	}
+	/**
+	 * @author jomin398
+	 * @see {@link https://github.com/takahirox} original author
+	 * @see {@link ZipLoader} This function is wrap of Ziploder
+	 * @param {String} url zip file url.
+	 * @param {String} exts model file extension.
+	 * @returns {{urls: Array<String>, manager: ZipLoadingManager}}
+	 */
+	uncompress(url, exts, onLoad, onProgress, onError) {
+		const self = this;
+		if (url.match(/\.zip$/) === null) return Promise.resolve({
+			urls: [url],
+			manager: this
+		});
+		//arrayfy when given exts is String.
+		if (!Array.isArray(exts)) exts = [exts];
+		//Regexp for File Search using given extension;
+		const searchRegex = new RegExp(`(${exts.join('|').replace(/\./gm, '\\.')})`, 'i');
+		return new ZipLoader(this).load(url).then(function (zip) {
+			let files = zip.find(searchRegex);
+			if (files.length === 0) {
+				return Promise.reject(new self.#errors.ENOENT(exts, url));
+			}
+			self.setURLModifier(zip.urlResolver);
+			return {
+				urls: files,
+				manager: self
+			};
+		}).catch(onError);
+	};
+}
 export { ZipLoadingManager };
